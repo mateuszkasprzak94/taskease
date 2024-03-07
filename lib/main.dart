a8bac6d9353f8560d05573b0630aedd2f315d7bd
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-03-07 14:49:33 +0100

    Install Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:to_do/app/app.dart';
+import 'package:to_do/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const MyApp(),
   );
