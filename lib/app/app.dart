cc328bf9a91c265aa1430485c6265e64cda93055
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-03-07 14:10:48 +0100

    Create config file and implement main_production

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/app/core/config.dart';
+import 'package:to_do/app/features/pages/home_page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: Config.debugShowCheckedModeBanner,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage());
+  }
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:to_do/app/app.dart';
+import 'package:to_do/app/core/config.dart';
 
 void main() {
+  Config.appFlavor = Flavor.production;
+
   runApp(
     const MyApp(),
   );
