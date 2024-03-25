79c053a513c0388b54b5285b1c696ff0f1b4afed
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-03-25 19:51:47 +0100

    Add MultiBlocProvider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,17 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do/app/core/config.dart';
 import 'package:to_do/app/cubit/auth_cubit.dart';
+import 'package:to_do/app/domain/repositories/items_repository.dart';
 import 'package:to_do/app/features/auth_page/login_page/auth_page.dart';
 import 'package:to_do/app/features/pages/home_page/home_page.dart';
 import 'package:to_do/app/features/auth_page/welcome_page/welcome_page.dart';
+import 'package:to_do/app/features/pages/home_page/tab_screens/cubit/tab_screen_cubit.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit()..start(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit()..start(),
+        ),
+        BlocProvider(
+          create: (context) => TabScreenCubit(ItemsRepository())..start(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: Config.debugShowCheckedModeBanner,
         theme: ThemeData(
